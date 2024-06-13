81cf113faf8a9b51a4c76fa219ce3dea6badead1
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-13 12:07:56 +0900

    Delete 프로그래머스/0/181862. 세 개의 구분자 directory

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++
@@ -1,6 +0,0 @@
-class Solution {
-    public String[] solution(String myStr) {
-        String[] answer = myStr.replace("a", " ").replace("b", " ").replace("c", " ").trim().replaceAll("\\s+", " ").split(" ");
-        return answer[0].equals("") ? new String[] {"EMPTY"} : answer;
-    }
-}
